c180628a21ed8a4a81086396f0f9cd67b616eedb
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-01-09 23:16:12 +0700

    add: middleware useraccess

---
+++ app/Http/Middleware/UserAccess.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserAccess
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, $userRole): Response
+    {
+        if (auth()->user()->role == $userRole) {
+            return $next($request);
+        }
+
+        return response()->json(['Anda tidak memiliki akses pada halaman ini.']);
+    }
+}
