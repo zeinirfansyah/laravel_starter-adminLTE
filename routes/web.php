9775561c1a3273074224991db68867ffa5f6111e
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-01-09 23:20:52 +0700

    update: routes di web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +21,17 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Normal user Routes
+Route::middleware(['auth', 'user-access:user'])->group(function(){
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+});
+
+// Admin routes
+Route::middleware(['auth', 'user-access:admin'])->group(function(){
+    Route::get('/admin/home', [HomeController::class, 'adminHome'])->name('admin.home');
+});
+
+// Manager routes
+Route::middleware(['auth', 'user-access:manager'])->group(function(){
+    Route::get('/manager/home', [HomeController::class, 'managerHome'])->name('manager.home');
+});
