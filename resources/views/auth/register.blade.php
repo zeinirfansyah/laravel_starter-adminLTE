f260811839d09d25a80be0055c6e235928133182
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-02-01 09:29:21 +0700

    fix: error validation [register.blade.php]

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -88,7 +88,7 @@
               <div class="row mb-3">
                 <label for="avatar" class="col-md-4 col-form-label text-md-end">{{ __('Avatar')}}</label>
                 <input id="avatar" type="file" class="form-control @error('avatar') is-invalid @enderror" name="avatar">
-                @error('password')
+                @error('avatar')
                 <span class="invalid-feedback" role="alert">
                   <strong>{{ $message }}</strong>
                 </span>
