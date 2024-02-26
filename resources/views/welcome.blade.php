2c2c4ee319897344edbe5390c8ca45083076e200
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-02-26 23:24:17 +0700

    fix [layout]: home not logged in

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,16 @@
   @auth
     @includeif('customer.home')
   @else
-    <div class="alert alert-success" role="alert">
-      You are not logged in!
+  <div class="container">
+   <div class="row justify-content-center">
+    <div class="col-md-6">
+      <div class="card p-3">
+        <div class="alert alert-success" role="alert">
+          You are not logged in!
+        </div>
+      </div>
     </div>
+   </div>
+  </div>
   @endauth
 @endsection
